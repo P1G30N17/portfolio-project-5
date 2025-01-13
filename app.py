36a3d279af9e894ad5bc4e0fd92b7fb4a87f78dc
James King <jamesking.1711@gmail.com>
2025-01-13 10:35:31 +0000

    Added technical page.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pages.explore_page import explore_page_body
 from pages.predict_salary_page import predict_salary_page_body
 from pages.hypothesis_page import hypothesis_page_body
 from pages.summary_page import summary_page_body
+from pages.technical_page import technical_page_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Software Engineer Job Market") 
@@ -15,6 +16,7 @@ app.add_page("Project Summary", summary_page_body)
 app.add_page("Predict Salary", predict_salary_page_body)
 app.add_page("Explore the Global Job Market", explore_page_body)
 app.add_page("Project Hypothesis", hypothesis_page_body)
+app.add_page("Technical", technical_page_body)
 
 # Run the app
 app.run()
---
+++ pages/technical_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def technical_page_body():
+    st.write("### Technical")
+    st.info("Technical Steps")
